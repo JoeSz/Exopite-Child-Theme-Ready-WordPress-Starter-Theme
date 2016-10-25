0e90f8b1ec31b44c226e8f9a71b7af9ab8d6132f
Joe <ujbeszel@gmail.com>
2016-10-25 12:05:19 +0200

    Add class container wrapper for whole header
    not only for nav

--- header.php
+++ header.php
@@ -24,37 +24,41 @@ tha_html_before();
 <body <?php body_class(); ?>>
 <?php tha_body_top(); ?>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'exopite' ); ?></a>
+	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'markatustools' ); ?></a>
 	<?php tha_header_before(); ?>
 	<header id="masthead" class="site-header" role="banner">
-		<?php tha_header_top(); ?>
-		<div class="site-branding">
-			<?php
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif;
+        <div class="container">
+    		<?php tha_header_top(); ?>
+    		<div class="site-branding">
+                <div class="row">
+                    <div class="col-md-12">
+            			<?php
+            			if ( is_front_page() && is_home() ) : ?>
+            				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+            			<?php else : ?>
+            				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+            			<?php
+            			endif;
 
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
-			endif; ?>
-		</div><!-- .site-branding -->
+            			$description = get_bloginfo( 'description', 'display' );
+            			if ( $description || is_customize_preview() ) : ?>
+            				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
+            			<?php
+            			endif; ?>
+                    </div>
+                </div>
+    		</div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<div class="container">
+    		<nav id="site-navigation" class="main-navigation" role="navigation">
 				<div class="row">
 					<div class="col-md-12">
-						<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'exopite' ); ?></button>
+						<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'markatustools' ); ?></button>
 						<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 					</div>
 				</div>
-			</div>
-		</nav><!-- #site-navigation -->
-		<?php tha_header_bottom(); ?>
+    		</nav><!-- #site-navigation -->
+    		<?php tha_header_bottom(); ?>
+        </div><!-- .container -->
 	</header><!-- #masthead -->
 	<?php tha_header_after(); ?>
 	<?php tha_content_before(); ?>
